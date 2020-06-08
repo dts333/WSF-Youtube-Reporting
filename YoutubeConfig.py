efb13ce071ee75785a9d6f9c75739b0998bb0b59
Danny Swift <dswift333@gmail.com>
2020-06-08 12:57:21 -0400

    added cool jobs dict

--- YoutubeConfig.py
+++ YoutubeConfig.py
@@ -6,13 +6,40 @@ Created on Wed Sep  4 14:10:01 2019
 @author: DannySwift
 """
 
-APIKEY = ''
-VIDSTATS = ['views', 'average_percentage_viewed']
-VIDSTATSTITLES = ['views', 'averageViewPercentage',]
-YT_ROWS = ['Views', 'Views from Home', 'Views from Subscriptions', 
-           'Suggested Videos', 'Channel Pages', 'Notifications']
-#relativeRetentionPerformance is throwing a 400
-CHANNELSTATS = ['traffic_source_type']
-DATA_DIRECTORY = 'YoutubeData'
-WSFID = 'UCShHFwKyhcDo3g7hr4f1R8A'
-YDEID = 'PLKy-B3Qf_RDVL6Z_CmgKf0tAbpXTua9mV'
+APIKEY = "AIzaSyD8Ib7mNFAFXlgmXh9YqcnCb2wxwh-K5bg"
+VIDSTATS = ["views", "average_percentage_viewed"]
+VIDSTATSTITLES = [
+    "views",
+    "averageViewPercentage",
+]
+YT_ROWS = [
+    "Views",
+    "Views from Home",
+    "Views from Subscriptions",
+    "Suggested Videos",
+    "Channel Pages",
+    "Notifications",
+]
+# relativeRetentionPerformance is throwing a 400
+CHANNELSTATS = ["traffic_source_type"]
+DATA_DIRECTORY = "YoutubeData"
+WSFID = "UCShHFwKyhcDo3g7hr4f1R8A"
+YDEID = "PLKy-B3Qf_RDVL6Z_CmgKf0tAbpXTua9mV"
+
+COOL_JOBS = {
+    "W8E9_R0EWDs": "The Bird Whisperer",
+    "Gib7IFdUnQs": "The Leech Guy",
+    "cp7C2AZhceE": "Robot Runner",
+    "JurS57yz2Ng": "Exercise Brain Changer",
+    "mbvq6-bqgXs": "Hubble Doctor",
+    "ryj5d2DjMlI": "Galaxy Explorer",
+    "Ldww6EIMAbM": "Cave Explorer",
+    "XLF-4c1XTJ4": "Doctor Bugs",
+    "YpbLa8BGv6E": "Alien Hunter",
+    "lQenUMoIufo": "Fossil Hunters",
+    "zZSmo2T_otg": "Da Vinci Detective",
+    "CyG3QZlgAgs": "Daredevil",
+    "Y1biZo9q4ck": "Human-Robot Investigator",
+    "_ePLqrHUhaw": "Nano Examiner",
+    "BCH6Iei-xcw": "Robot Senser",
+}
